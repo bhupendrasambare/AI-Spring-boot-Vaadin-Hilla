2143803a47d043231e9ccc8586cbb8b2aa78f79c
bhupendrasambare <bhupendrasam1404@gmail.com>
2024-08-06 01:37:35 +0530

    OpenApi error spotted

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -4,6 +4,8 @@ import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.theme.Theme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * The entry point of the Spring Boot application.
@@ -20,4 +22,8 @@ public class Application implements AppShellConfigurator {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
